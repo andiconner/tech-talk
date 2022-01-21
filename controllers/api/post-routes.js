44e365f1527dc77427131fcd2c146b7b2fd8ef73
Andi Conner <andiconner@icloud.com>
2022-01-21 10:18:27 -0500

    comment model and routes created

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 // In a query to the post table, we would like to retrieve not only 
 //information about each post, but also the user that posted it
 
@@ -9,6 +9,14 @@ router.get('/', (req, res) => {
         attributes: ['id', 'post_url', 'title', 'created_at'],
         order: [['created_at', 'DESC']],// This will ensure that the latest posted articles will appear first
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -29,6 +37,14 @@ router.get('/:id', (req, res) => {
         },
         attributes: ['id', 'post_url', 'title', 'created_at'],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
