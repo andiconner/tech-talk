c83111dff23dac57298b455faea3c2da80e3bdc3
Andi Conner <andiconner@icloud.com>
2022-01-21 13:10:05 -0500

    homepage

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+const path = require('path');
 const express = require('express');
-const controllers = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public'))); //express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets
 
 // turn on the routes
-app.use(controllers);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
