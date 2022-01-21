c2e6a62e5a1e81f0c9364b234257108176e82bb1
Andi Conner <andiconner@icloud.com>
2022-01-20 19:47:55 -0500

    post model created

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+// connection to MySQL we stored in the connection.js file as well as Model and Datatypes we'll use from the sequelize package.
+const { Model, DataTypes } = require('sequelize'); 
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+//create fields/colums for Post model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post; 
+//to make the Post model accessible to other part of the application
