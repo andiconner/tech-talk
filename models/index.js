adeda5de532a482874c2c451eb70b0c2c4e27020
Andi Conner <andiconner@icloud.com>
2022-01-20 20:53:16 -0500

    created post-routes

--- models/index.js
+++ models/index.js
@@ -3,6 +3,13 @@ const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
 
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
 module.exports = { User, Post, Comment };
